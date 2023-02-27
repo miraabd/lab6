ac5a166ec2b519e79ee8a80d264952034a032eeb
Mira Abd <mira.abd.2000@gmail.com>
2023-02-27 20:05:17 +0600

    commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//problem 3
+function area(r){
+    return 3.14*r*r
+}
+
+//problem 11
+function evenOrodd(num){
+    if (num % 2 == 0){
+        console.log("Given number is even")
+    }
+    else{
+        console.log("Given number is odd")
+    }
+}
+
+//problem 7
+function product(x, y, z){
+    return x*y*z
+}
